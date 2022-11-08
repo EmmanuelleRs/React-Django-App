f54e82e69ad595429dc9005d9c53e865a01264af
Oscar Raya <oscar.raya@mrm.com>
2022-11-07 21:16:49 -0600

    Initial commit

---
+++ apps/frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+    import ReactDOM from "react-dom/client";
+    
+    const app = ReactDOM.createRoot(document.getElementById('react-app'));
+    
+    app.render(
+        <>
+        <h1>REACT DJANGO APP</h1>
+        </>
+    )
---
+++ apps/frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "../../static/js/"),
+        filename: "frontend.js",
+    },
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "babel-loader",
+            },
+        },
+        {
+            test: /\.css$/i,                                                                                                                                                             
+            use: ["style-loader", "css-loader"],                                                                                                                          
+        }, 
+        ],
+    },
+    optimization: {
+        minimize: true,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+        "process.env": {
+            // This has effect on the react lib size
+            'process.env.NODE_ENV' : JSON.stringify('production')
+        },
+        }),
+    ],
+};
+
